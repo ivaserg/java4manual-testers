5592a568f66c9ef5f1103ef4e72f11a2a5bb63ad
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-11-05 15:20:23 +0300

    Add maven POM and stub application.

---
+++ src/main/java/com/db/edu/PrinterApp.java
@@ -0,0 +1,7 @@
+package com.db.edu;
+
+public class PrinterApp {
+    public static void main( String... args ) {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/db/edu/PrinterAppTest.java
@@ -0,0 +1,9 @@
+package com.db.edu;
+
+//TODO Refactor to OOP testable design.
+public class PrinterAppTest {
+    public void shouldPrintWhenNormalFlowStringParameter() {
+        PrinterApp sut = new PrinterApp();
+        sut.main();
+    }
+}
