e2a15841604c7f4928b327fe3c5cff1eb348394e
ivaserg <ivanov.svv@gmail.com>
2016-11-18 11:59:46 +0300

    Rename PrinterApp to EtlApp
    (cherry picked from commit 9363084)

--- src/main/java/com/db/edu/EtlApp.java
+++ src/main/java/com/db/edu/EtlApp.java
@@ -3,8 +3,8 @@ package com.db.edu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PrinterApp {
-    private static final Logger logger = LoggerFactory.getLogger(PrinterApp.class);
+public class EtlApp {
+    private static final Logger logger = LoggerFactory.getLogger(EtlApp.class);
 
     public static void main(String... args) {
         logger.debug("Entering main method with args {}", args);
