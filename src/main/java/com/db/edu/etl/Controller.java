adbe995f213004667e52c08930d11d8f34964262
ivaserg <ivanov.svv@gmail.com>
2016-11-21 11:04:42 +0300

    + Add ETL methods with mocks.  + Add call of etl methods in main class

--- src/main/java/com/db/edu/etl/Controller.java
+++ src/main/java/com/db/edu/etl/Controller.java
@@ -1,6 +1,16 @@
 package com.db.edu.etl;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.db.edu.etl.RecordType.*;
+
 public class Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
     public static String[] transform(RecordType recordType, String rawDataRecord) {
         switch (recordType) {
             case EIS1_DATA_FILE: {
@@ -11,5 +21,16 @@ public class Controller {
             }
             default: return null;
         }
+    }
+
+    public static String extract(RecordType recordType) {
+        valueOf("EIS3");
+
+        return null;
+    }
+
+    public static boolean load(String[] data) {
+
+        return false;
     }
 }
